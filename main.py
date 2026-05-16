6caa8397fe6d6fce2f7765c8abcceaf70dbae04c
InvaderJT <invaderjt@gmail.com>
2026-05-16 13:22:42 -0400

    fixed player placement

--- main.py
+++ main.py
@@ -19,9 +19,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = (game_clock.tick(60))/1000
-        player.draw(screen)
+        
 
 
 
